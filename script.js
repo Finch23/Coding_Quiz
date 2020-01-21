17662915712719b5848c8c899832407b2b274e2f
Nicholas Finch <nicfinch223@gmail.com>
2020-01-21 17:57:55 -0500

    initial files, good start

---
+++ script.js
@@ -0,0 +1,21 @@
+var start = document.getElementById('start');
+
+var questions [
+    {
+        question: 'Commonly used data types DO NOT include:',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts'
+    },
+    {
+        question: 'The condition in an if / else statement is enclosed with ____.',
+        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+        answer: 'parentheses'
+    },
+    {
+        question: 'What tag is used to define the bottom of an HTML doc',
+        choices: ['p', 'h1-h6', 'footer', 'body'],
+        answer: 'footer'   
+    }
+    {
+
+    }
